762794313416871da31deafa1977aa474ed1297a
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-14 08:55:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,5 +20,14 @@ return "done";
 }
 
 function doWhile(int){
-  
+  var i = 0;
+
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+  do {
+   console.log("I run once regardless");
+}  while (incrementVariable() < 5);
 }
