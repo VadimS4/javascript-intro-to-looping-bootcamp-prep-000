359d21b5a78b07c020bc2d3f998a69c51c0b3dfb
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-14 08:16:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array){
   for (let i = 0; i < 26; i++){
-    
+    if(i == 1){
+      return "I am 1 strange loop."
+    }
+    else if(i > 1){
+      return "I am ${i} strange loops."
+    }
   }
 }
